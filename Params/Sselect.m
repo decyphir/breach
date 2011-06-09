c0c2813314552d5e57ee62fff4a81d687420d0d8
Alex Donze <alex@decyphir.com>
2011-06-09 14:43:06 +0200

    added Sselect.m

---
+++ Params/Sselect.m
@@ -0,0 +1,24 @@
+function Sn = select(S, kn)
+  
+  Sn = [];
+ 
+  field_list_copy = {'dim', 'ParamList', 'DimX', 'DimP', 'props_names', 'props'};
+
+  for i = 1:numel(field_list_copy)    
+    if isfield(S, field_list_copy{i})
+      Sn.(field_list_copy{i}) = S.(field_list_copy{i});
+    end
+  end
+  
+  field_list_select= {'pts', 'epsi', 'XS0', 'Xf', 'ExpaMax', 'XSf','traj', ...
+               'selected', 'props_values','etraj'};
+  
+  
+  for i = 1:numel(field_list_select)    
+    if isfield(S, field_list_select{i})
+      Sn.(field_list_select{i}) = S.(field_list_select{i})(:,kn);
+    end
+  end
+  
+  
+ 
