500a901dc593127dca760f3e8d781598047c3be5
Alex Donze <alex@decyphir.com>
2012-01-03 16:52:27 +0100

    purged some Sxxxx.m things and got rid of private foldes

---
+++ Params/SConcat.m
@@ -0,0 +1,23 @@
+function S= SConcat(S,S2)
+
+   field_list_copy = {'props_names', 'props',  'time_mult'};
+ 
+  for i = 1:numel(field_list_copy)    
+    if isfield(S2, field_list_copy{i})
+      S.(field_list_copy{i}) = S2.(field_list_copy{i});
+    end
+  end
+ 
+  field_list= {'pts', 'epsi', 'XS0', 'Xf', 'ExpaMax', 'XSf','traj', ...
+               'selected','props_values', };
+    
+  for i = 1:numel(field_list)
+  
+    if ((isfield(S,field_list{i}))&&(isfield(S2,field_list{i})))
+      if (numel(S.(field_list{i}))==0)
+        S.(field_list{i}) = S2.(field_list{i});       
+      else
+        S.(field_list{i}) = [S.(field_list{i}) S2.(field_list{i})];     
+      end
+    end    
+  end
