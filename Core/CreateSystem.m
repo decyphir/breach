686b21a387c04801983bf9be1a2324f3fc4ed151
Alex Donze <alex@decyphir.com>
2012-01-10 10:01:46 +0100

    modif in CreateSystem

--- Core/CreateSystem.m
+++ Core/CreateSystem.m
@@ -1,13 +1,20 @@
-function CreateSystem(vars, params)
-      
+function Sys = CreateSystem(vars, params, p0)
+% CREATESYSTEM creates a dynamicless system      
+
+
   Sys.DimX = numel(vars);
   Sys.DimU =0; 
   Sys.DimP = numel(vars)+numel(params); 
   Sys.ParamList = {vars{:} params{:}};
-
-  Sys.x0 = zeros(1,numel(vars));
-  Sys.p = zeros(1, Sys.DimP);
-
+  Sys.x0 = zeros(1,numel(vars));  
+  
+  if (~exist('p0'))
+      Sys.p = zeros(1, Sys.DimP);
+  else
+      Sys.p = p0;
+  end
+  
+  % dynamicless system
   Sys.type = 'traces';
   
   assignin('base','Sys',Sys);
