29e53af2280243aa9928179a6c71c3d6966f8d3b
Alex Donze <alex@decyphir.com>
2012-06-06 18:46:07 +0200

    new cmap

--- Plots/m_src/prop_cmap.m
+++ Plots/m_src/prop_cmap.m
@@ -1,19 +1,20 @@
-function cm = prop_cmap2(n0,n1)
+function cm = prop_cmap(n0,n1)
   
   if nargin == 0
     n0 = 128;
     n1 = 128;
   end
  
- 
-  o0 = ones(n0,1);
-  c0 = linspace(0,.5, n0)';
- 
-  o1 = ones(n1,1);
-  c1 = linspace(.5,0, n1)';
- 
-  cm =   [ o0 c0 c0 ; 
-           c1 o1 c1];
+  r0 = linspace(0,0.5, n0)'; %ones(n0,1);
+  v0 = linspace(1,.5, n0)';
+  b0 = zeros(n0, 1);
+  
+  r1 = linspace(0.5,1, n1)'; % o1 = ones(n1,1);
+  v1 = linspace(.5,0, n1)'; 
+  b1 = zeros(n1, 1);
+  
+  cm =   [ r0 v0 b0 ; 
+           r1 v1 b1];
  
   colormap(cm);
   
