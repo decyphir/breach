65b62c8cec9b09475a36615b601f444b1eadfe91
Alex Donze <alex@decyphir.com>
2012-06-27 16:41:05 +0200

    files for qr sequences

---
+++ Params/QuasiRefine.m
@@ -0,0 +1,18 @@
+function Sh = SobolRefine(S, nb)
+% QUASIREFINE  Sample quasi-uniformly a parameter set 
+% 
+% Synopsis:  Ph = QuasiRefine(S, nb)
+%  
+% Example: 
+%
+%   CreateSystem;
+%   P = CreateSampling(Sys); % Create default parameter set for system Sys
+%   Ph = SobolRefine(P, 1000); % Sample with 1000 points
+%  
+%   SplotBoxPts(P); % Parameter set before sampling 
+%   SplotPts(Ph);   % plots the generated points
+%
+% TODO optionalize the sequence used - so far Sobol
+%  
+  
+  Sh = SobolRefine(S,nb) 
