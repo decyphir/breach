78738e8ffb3eed86987ce2c80317ab69d5d784a0
Nicolas <Nicolas.Mobilia@imag.fr>
2012-12-15 17:51:08 +0100

    add GetEpsi.m

---
+++ Params/GetEpsi.m
@@ -0,0 +1,69 @@
+function EpsiValues = GetEpsi(P,ParamList)
+% GETEPSI get the epsi of parameters in a parameter set. The function
+% return an empty set if a parameter name or a parameter indice is not
+% valid.
+%
+% Synopsis: EpsiValues = GetEpsi(P, ParamList)
+%
+%See also GetParam
+
+%
+% Example (for Lorenz84 system):
+%
+%    CreateSystem;
+%    P = CreateParamSet(Sys, {'a', 'b'}, [0 10; 0 5]);
+%    Pr = Refine(P, 3);
+%    val = GetParam(Pr, 'a');
+%    val = GetParam(Pr, 'F'); % get an array 1x9 filled with the value of F
+%    val = GetParam(Pr, 'blah'); % return an empty array
+%    val = GetParam(Pr, {'a', 'b'});
+%    val = GetParam(Pr, {'b', 'a'});
+%
+
+
+    % case 1 : the ParamList argument is an array char of one parameter
+    % name
+    if ischar(ParamList)
+        ind = FindParam(P,ParamList);
+        if(ind<=size(P.pts,1)) %check parameter existence
+            ind = P.dim(P.dim==ind);
+            EpsiValues = P.epsi(ind,:);
+        else
+            EpsiValues = [];
+        end
+        return
+    end
+
+    % case 2 : the ParamList parameter is a list of integer
+    if isnumeric(ParamList)
+        %check for an error in the list
+        if ~isempty(find(ParamList>size(P.pts,1),1)) || ...
+                                    ~isempty(find(ParamList<=0,1))
+            EpsiValues = [];
+            return
+        end
+        %initialize ParamValues
+        EpsiValues = zeros(numel(ParamList),size(P.pts,2));
+        %copy the values
+        for i = 1:numel(ParamList)
+            ind = P.dim(P.dim==ParamList(i));
+            EpsiValues(i,:) = P.epsi(ind,:);
+        end
+        return
+    end
+
+    % case 3 : the ParamList parameter is a list of parameter name
+    inds = FindParam(P,ParamList);
+    %initialization
+    if ~isempty(find(inds>size(P.pts,1),1))
+        EpsiValues = [];
+        return;
+    else
+        EpsiValues = zeros(numel(ParamList),size(P.pts,2));
+    end
+    for i = 1:numel(ParamList)
+        ind = P.dim(P.dim==inds(i));
+        EpsiValues(i,:) = P.epsi(ind,:);
+    end
+
+end
