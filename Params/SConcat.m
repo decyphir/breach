7952eb97c68619353373e66bbf35b4624eea27d1
Nicolas <Nicolas.Mobilia@imag.fr>
2012-12-20 19:09:00 +0100

    indentation update

--- Params/SConcat.m
+++ Params/SConcat.m
@@ -1,56 +1,55 @@
 function S= SConcat(S,S2)
-% SCONCAT concat two parameter sets 
-% 
-% Synopsis: P = SConcat(P1, P2) 
+% SCONCAT concat two parameter sets
 %
-%  Tries to concat all compatible fields in P2 to those in P1. Basically add points and trajectories of P2 in P1. 
+% Synopsis: P = SConcat(P1, P2)
 %
-  
-  
-  field_list_copy = {'props_names', 'props',  'time_mult'};
- 
-  for i = 1:numel(field_list_copy)    
+%  Tries to concat all compatible fields in P2 to those in P1. Basically add points and trajectories of P2 in P1.
+%
+
+
+field_list_copy = {'props_names', 'props',  'time_mult'};
+
+for i = 1:numel(field_list_copy)
     if isfield(S2, field_list_copy{i})
-      S.(field_list_copy{i}) = S2.(field_list_copy{i});
+        S.(field_list_copy{i}) = S2.(field_list_copy{i});
     end
-  end
- 
-  field_list= {'pts', 'epsi', 'XS0', 'Xf', 'ExpaMax', 'XSf', ...
-               'selected','props_values'};
-  
-  for i = 1:numel(field_list)
+end
+
+field_list= {'pts', 'epsi', 'XS0', 'Xf', 'ExpaMax', 'XSf', ...
+    'selected','props_values'};
+
+for i = 1:numel(field_list)
     
     if ((isfield(S,field_list{i}))&&(isfield(S2,field_list{i})))
-      if (numel(S.(field_list{i}))==0)
-        S.(field_list{i}) = S2.(field_list{i});       
-      else
-        S.(field_list{i}) = [S.(field_list{i}) S2.(field_list{i})];       
-      end
-    end    
-  end
+        if (numel(S.(field_list{i}))==0)
+            S.(field_list{i}) = S2.(field_list{i});
+        else
+            S.(field_list{i}) = [S.(field_list{i}) S2.(field_list{i})];
+        end
+    end
+end
 
-  if (isfield(S, 'traj')&&(isfield(S2,'traj')))
-
-    %%% TEMPORARY FIX 
+if (isfield(S, 'traj')&&(isfield(S2,'traj')))
+    
+    %%% TEMPORARY FIX
     if size(S.traj,1) >1
-      S.traj = S.traj';
+        S.traj = S.traj';
     end
     
     if size(S2.traj,1) >1
-      S2.traj = S2.traj';
+        S2.traj = S2.traj';
     end
-        
+    
     if (~isfield(S, 'traj_ref'))
-      S.traj_ref= 1:numel(S.traj);
+        S.traj_ref= 1:numel(S.traj);
     end
     
     if (~isfield(S2, 'traj_ref'))
-      S2.traj_ref= 1:numel(S2.traj);
+        S2.traj_ref= 1:numel(S2.traj);
     end
     
-    S.traj_ref = [S.traj_ref S2.traj_ref+numel(S.traj)];  
+    S.traj_ref = [S.traj_ref S2.traj_ref+numel(S.traj)];
     S.traj = [S.traj S2.traj];
     
-  end
-      
-  
+end
+
