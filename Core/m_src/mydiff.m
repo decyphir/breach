5144a242e1d0bf4ebb4d43b331c41a662fdbd089
Nicolas <Nicolas.Mobilia@imag.fr>
2013-02-22 18:51:29 +0100

    doc update + iron: local sensi of phi

--- Core/m_src/mydiff.m
+++ Core/m_src/mydiff.m
@@ -1,31 +1,46 @@
-function st = mydiff(st, param)
-% function df_st = mydiff(f_st, param, param_list)
-%
-%  Computes the symbolic derivative of f_st w.r.t p, where f_st is an
+function f_st = mydiff(f_st, param)
+%MYDIFF Computes the symbolic derivative of f_st w.r.t p, where f_st is an
 %  expression involving symbols in param_list.
 %
+% Synopsis: df_st = mydiff(f_st, param)
+%
+% Input:
+%  - f_st  : string containing the mathematical expression to differentiate
+%  - param : string containing the variable w.r.t. which f_st is
+%            differentiated
+%
+% Output:
+%  - df_st : string containing the mathematical expression of
+%            d f_st/d param
+%
+% Example:
+%
+%
+%
+%See also: DiffFormula
+%
 
-st = regexprep(st,'\.\*','*');
-st = regexprep(st,'\./','/');
-st = regexprep(st,'\.\^','\^');
+f_st = regexprep(f_st,'\.\*','*');
+f_st = regexprep(f_st,'\./','/');
+f_st = regexprep(f_st,'\.\^','\^');
 
-if exist('mydiff_mex') % NM: replace by exist(('mydiff_mex','class') ?
-    st = mydiff_mex(st,param);
-    % st = regexprep(st,'\*','\.\*');
-    % st = regexprep(st,'/','\./');
-    % st = regexprep(st,'^','\.\^');
+if exist('mydiff_mex','file')
+    f_st = mydiff_mex(f_st,param);
+    % f_st = regexprep(f_st,'\*','\.\*');
+    % f_st = regexprep(f_st,'/','\./');
+    % f_st = regexprep(f_st,'^','\.\^');
     return;
 end
 
 try
     syms x;
-    st = diff(st,param);
+    f_st = diff(f_st,param);
 catch
     error('mydiff:diffError','Problem: maybe Symbolic toolbox not present.');
 end
 
-%  st = regexprep(st,'\*','\.\*');
-%  st = regexprep(st,'/','\./');
-%  st = regexprep(st,'^','\.\^');
+%  f_st = regexprep(f_st,'\*','\.\*');
+%  f_st = regexprep(f_st,'/','\./');
+%  f_st = regexprep(f_st,'^','\.\^');
 
 end
