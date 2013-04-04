1d5c656d27752a09107ddb35f10b7092d7c4872b
Nicolas <Nicolas.Mobilia@imag.fr>
2013-04-04 16:15:19 +0200

    doc update

--- Params/SobolRefine.m
+++ Params/SobolRefine.m
@@ -1,4 +1,4 @@
-function Ph = SobolRefine(P, nb, step)
+function P = SobolRefine(P, nb, step)
 % SOBOLREFINE  Sample quasi-uniformly a parameter set using Sobol sequence
 %
 % Credit:  John Burkardt, 2006
@@ -10,15 +10,17 @@ function Ph = SobolRefine(P, nb, step)
 %   CreateSystem;
 %   P = CreateParamSet(Sys,{'F','G'},[1,100;0,5]);
 %   Ph = SobolRefine(P, 1000); % Sample with 1000 points
+%   
+%   Ph2 = Refine(P, 2); % 4 parameters sets
+%   Ph2 = SobolRefine(Ph2, 250) % also 1000 parameter sets
 %
 %   SplotBoxPts(P); % Parameter set before sampling
-%   SplotPts(Ph);   % plots the generated points
+%   SplotPts(Ph2);   % plots the generated points
 %
 %See also QuasiRefine Refine RandomLogRefine
 %
 
 if(nb<=1)
-    Ph = P;
     return ;
 end
 
@@ -32,13 +34,12 @@ end
 r = i4_sobol_generate(dim_num, nb, step);
 r = kron(r, ones(1,size(P.pts,2)));
 
-width = 2*P.epsi;
-inf = P.pts(P.dim,:)-P.epsi;
-Ph = P;
-Ph.pts = repmat(P.pts,[1 nb]);
-Ph.pts(P.dim,:) = repmat(width,[1 nb]).*r+repmat(inf,[1 nb]);
+width = 2*P.epsi;   % NM : TODO make modifications to avoid parameter sets out of the initial interval
+inf = P.pts(P.dim,:)-P.epsi;   % NM : TODO make modifications to avoid parameter sets out of the initial interval
+P.pts = repmat(P.pts,[1 nb]);
+P.pts(P.dim,:) = repmat(width,[1 nb]).*r+repmat(inf,[1 nb]);
 
-Ph.epsi = repmat(Ph.epsi,[1 nb])/(floor(nb^(1/dim_num)));
+P.epsi = repmat(P.epsi,[1 nb])/(floor(nb^(1/dim_num)));
 
 % Wrong : the dimension of epsi differs from the dimension of pts
 %Ph.epsi = kron(Ph.epsi,ones(1,size(Ph.pts,2)))/(floor(nb^(1/dim_num)));
@@ -48,7 +49,7 @@ Ph.epsi = repmat(Ph.epsi,[1 nb])/(floor(nb^(1/dim_num)));
 %Sh.epsi = kron(Sh.epsi, ones(1,size(Sh.pts,2)))/nb;
 
 if (isfield(P,'selected'))
-    Ph.selected = zeros(1, size(P.pts,2));
+    P.selected = zeros(1, size(P.pts,2));
 end
 
 end
