024dfac9faa04963ba392e3c62e3145c1f979319
Nicolas <Nicolas.Mobilia@imag.fr>
2013-09-17 22:38:05 +0200

    ident

--- InitBreach.m
+++ InitBreach.m
@@ -21,28 +21,28 @@ addpath( [dr_ filesep 'Toolboxes' filesep 'sundials' filesep 'sundialsTB' filese
 
 %% Init BreachGlobOpt options and fourre-tout global variable
 
-if (exist('BreachGlobOpt.mat'))  
-  load BreachGlobOpt;
-  
-  % Convert BreachGlobOpt into global
-  BreachGlobOptTmp = BreachGlobOpt;
-  clear BreachGlobOpt;
-  global BreachGlobOpt;
-  BreachGlobOpt = BreachGlobOptTmp; 
-  clear BreachGlobOptTmp;
-  BreachGlobOpt.RobustSemantics = 0 ; % 0 by default, -1 is for left time robustness, +1 for right, inf for sum ?
-
-else 
-
-  if (~exist('BreachGlobOpt','var'))
+if exist('BreachGlobOpt.mat')
+    load BreachGlobOpt;
+    
+    % Convert BreachGlobOpt into global
+    BreachGlobOptTmp = BreachGlobOpt;
+    clear BreachGlobOpt;
     global BreachGlobOpt;
-    BreachGlobOpt.breach_dir = dr_;  
-  end
-  
-  if (~isfield(BreachGlobOpt,'RobustSemantics'))        
-    BreachGlobOpt.RobustSemantics = 0;     
-  end
-  
+    BreachGlobOpt = BreachGlobOptTmp;
+    clear BreachGlobOptTmp;
+    BreachGlobOpt.RobustSemantics = 0 ; % 0 by default, -1 is for left time robustness, +1 for right, inf for sum ?
+    
+else
+    
+    if ~exist('BreachGlobOpt','var')
+        global BreachGlobOpt;
+        BreachGlobOpt.breach_dir = dr_;
+    end
+    
+    if ~isfield(BreachGlobOpt,'RobustSemantics')
+        BreachGlobOpt.RobustSemantics = 0;
+    end
+    
 end
 
 cd(cdr_);
