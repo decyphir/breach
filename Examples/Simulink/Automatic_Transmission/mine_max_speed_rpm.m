327973b7e62fc1a8fab84dfade450830d894191f
Alex Donze <alex@decyphir.com>
2015-06-19 11:47:01 -0700

    mining example

--- Examples/Simulink/Automatic_Transmission/mine_max_speed_rpm.m
+++ Examples/Simulink/Automatic_Transmission/mine_max_speed_rpm.m
@@ -18,16 +18,16 @@ prop_opt.p_tol      = [1 1];
   
 %% System and falsification parameters
 falsif_opt.params = {'throttle_u0' ... ,
-                     'brake_u0'... 
+%                     'brake_u0'... 
                     };
 falsif_opt.ranges = [0 100; ...
-                     0 325; ...   
+%                     0 325; ...   
 ];
 falsif_opt.nb_init = 10;
 falsif_opt.nb_iter = 1000;
 falsif_opt.nb_max_call = 1000;
 
 %% Max number of mining iterations
-iter_max= 100;
+iter_max= 10;
 [p, rob, Pr] = ReqMining(Sys, phi_template, falsif_opt, prop_opt, iter_max);
 Psave(Sys, 'Pr'); % run Breach(Sys) to explore the successive counter-examples 
