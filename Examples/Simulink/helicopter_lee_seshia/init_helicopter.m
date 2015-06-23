e925da3950341a4900e33d95ac5fae3552e25045
Alex Donze <alex@decyphir.com>
2015-06-23 15:29:47 -0700

    moved bio examples

--- Examples/Simulink/helicopter_lee_seshia/init_helicopter.m
+++ Examples/Simulink/helicopter_lee_seshia/init_helicopter.m
@@ -1,12 +1,17 @@
 
-
+%% Name of the Simulink model 
 mdl = 'helicopter';
 
+%% Parameters needed to run the model 
 K = 10;
 i = 0; 
 a = 1;
 
-
+%% Interface with Breach 
 Sys = CreateSimulinkSystem(mdl)
+
+%% Default 
 Sys.tspan = 0:.01:4;
+
+%% Default value for input signal
 Sys = SetParam(Sys, 'psi_u0', 10);
