ad3c77911d3ee9dc9cb221fec14519ad96deafa1
Alexandre Donze <alex@decyphir.com>
2017-03-30 00:12:59 +0200

    fix for long filenames

--- Core/m_src/LoadLogFolder.m
+++ Core/m_src/LoadLogFolder.m
@@ -1,14 +1,15 @@
 function B =  LoadLogFolder(folder_name)
+% LoadLogFolder look into folder folder_name for logged traces and returns a 
+% breach system concatenating all those found. 
 
 if ~isempty(folder_name)
-    dd = ls([folder_name filesep 'Br*.mat']);
-    load([folder_name filesep dd(1,:)]);
+    dd = dir([folder_name filesep 'Br*.mat']);
+    load([folder_name filesep dd(1).name]);
     B = Br.copy();
     for id= 2:size(dd, 1)
-        load([folder_name filesep dd(id,:)]);
+        load([folder_name filesep dd(id).name]);
         B.Concat(Br);
-    end
-    
+    end    
 end
 
 end
