a9ef0da26ed955ec543a8ae556e9d5af3047b492
Xiaoqing Jin <sunqxj@gmail.com>
2018-01-09 16:44:27 -0800

    add the GUI test wrapper

--- Examples/Tests/GUITest/GUI_Test_snip.m
+++ Examples/Tests/GUITest/GUI_Test_snip.m
@@ -1,11 +1,19 @@
 BrDemo.InitAFC
 %%
-robot = java.awt.Robot;
+%robot = java.awt.Robot;
 %%
 % test to click the "Copy" button in the BreachGui
 BreachGui
 hBreach = findall(0, 'Tag', 'breach');
-hCopyBtn = findall(hBreach, 'String', 'Copy');
-posGUI = getpixelposition(hBreach);
-pos = getpixelposition(hCopyBtn, true); % Get the pixel coordination relative to the GUI
-jmouseemu(posGUI(1:2)+pos(1:2), 'normal')
+inputEmuWrapper(hBreach, 'button_copy_set', 0.5, 'normal');
+% validate
+hWorkSetListBox = findall(hBreach, 'Tag', 'working_sets_listbox');
+selected_name = hWorkSetListBox.String{hWorkSetListBox.Value};
+added_name = hWorkSetListBox.String{end};
+assert(~isempty(strfind(added_name, selected_name)))
+
+%% test the keyboard
+inputEmuWrapper(hBreach, 'edit_rename', 0, 'key_normal', 'testName\ENTER');
+% validate
+assert(any(strcmp(hWorkSetListBox.String, 'testName')))
+%%
