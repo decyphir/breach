1711e570db5d2d929ad62d8329cd3b08be8a0566
alex <alex@decyphir.com>
2019-11-15 13:34:51 +0100

    1.7.beta2

--- @STL_Formula/STL_ExtractSignals.m
+++ @STL_Formula/STL_ExtractSignals.m
@@ -21,31 +21,35 @@ end
 sreserved = {'alw_', 'ev_','until_'};
 signals = setdiff(signals, sreserved);
 
-%% params
-params = {};
-[~,~, ~, matches, tokens] = regexp(st_phi, '(\<\w+\>)');
-for im=1:numel(matches)
-    varname = tokens{im}{1};
-    if isvarname(varname)&& ...
-            all(exist(varname) ~= [2 3 5 6])  % checks for m-files, mex files, builtin, p-files
-        params{end+1} = tokens{im}{1};
+
+if nargout>1
+    %% params
+    params = {};
+    [~,~, ~, matches, tokens] = regexp(st_phi, '(\<\w+\>)');
+    for im=1:numel(matches)
+        varname = tokens{im}{1};
+        if isvarname(varname)
+            if all(exist(varname) ~= [2 3 5 6])  % checks for m-files, mex files, builtin, p-files
+                params{end+1} = tokens{im}{1};
+            end
+        end
     end
-end
-
-reserved = [ sreserved signals  {'alw', 'ev', 'and', 'or', '=>', 'not', 'until', 't', ...
-    'abs', 'sin', 'cos', 'exp','tan', 'norm','sqrt'}];
-params = setdiff(params, reserved);
-params = unique(params);
-
-%% p0
-if nargout>=3
-    p0 = zeros(1,numel(params));
-    param_struct= get_params(phi);
-    for ip = 1:numel(params)
-        if isfield(param_struct, params{ip});
-            p0(ip) = param_struct.(params{ip});
-        else
-            p0(ip) =0;
+    
+    reserved = [ sreserved signals  {'alw', 'ev', 'and', 'or', '=>', 'not', 'until', 't', ...
+        'abs', 'sin', 'cos', 'exp','tan', 'norm','sqrt'}];
+    params = setdiff(params, reserved);
+    params = unique(params);
+    
+    %% p0
+    if nargout>=3
+        p0 = zeros(1,numel(params));
+        param_struct= get_params(phi);
+        for ip = 1:numel(params)
+            if isfield(param_struct, params{ip});
+                p0(ip) = param_struct.(params{ip});
+            else
+                p0(ip) =0;
+            end
         end
     end
 end
